0fae22b6c528031ccf8ea4ba9c46a22a1617ca9e
jeffreymrussell <jeffrey.russell@vwds.pt>
2024-02-06 10:13:10 +0000

    Updates to install to include monokai

--- install/install.sh
+++ install/install.sh
@@ -2,6 +2,7 @@
 
 ln ~/.config/vim/.vimrc ~/.config/.vimrc
 ln ~/.config/zsh/.zshenv_in_root ~/.zshenv
+curl -fLo ~/.vim/colors/molokai.vim --create-dirs https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +slient +VimEnter +PlugInstall +qall
 
