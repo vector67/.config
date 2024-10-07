5b6dc2a07385cde89f532ca20e84249d32554954
jeffreymrussell <jeffrey.russell@vwds.pt>
2024-10-07 21:02:05 +0100

    NVIM: add file name to lualine

--- nvim/lua/jeffrey/plugins/lualine.lua
+++ nvim/lua/jeffrey/plugins/lualine.lua
@@ -55,6 +55,9 @@ return {
         theme = my_lualine_theme,
       },
       sections = {
+        lualine_c = {
+          { "%f" }
+        },
         lualine_x = {
           {
             lazy_status.updates,
