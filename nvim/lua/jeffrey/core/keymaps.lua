b91af5ab37246d9ff131d7909df23b74039750c0
jeffreymrussell <jeffrey.russell@vwds.pt>
2024-10-21 21:04:11 +0100

    NVIM: window splits and tab movements

--- nvim/lua/jeffrey/core/keymaps.lua
+++ nvim/lua/jeffrey/core/keymaps.lua
@@ -17,7 +17,7 @@ keymap.set("n", "<leader>rms", ":MarkdownPreviewStop<cr>", {desc = "Stop markdow
 keymap.set("n", "<leader>rmo", ":!pandoc -f markdown -o \"<C-r>%<C-W>pdf\" --resource-path=/Users/etrnj73/vimwiki --template=eisvogel \"<C-r>%\"<cr><cr>:!open \"<C-r>%<C-W>pdf\"<cr><cr>", {desc = "Convert current file to pdf"})
 
 
-keymap.set("n", "<leader>/", ":noh<cr>", {desc = "Clear current search"})
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>ss", ":wa<cr>", {desc = "Save entire file"})
 keymap.set("n", "<leader>sp", ":setlocal spell spelllang=en_us<cr>", {desc = "Enable spell checking locally"})
 keymap.set("n", "<leader>ya", "ggvG$y", {desc = "Yank entire file"})
@@ -46,6 +46,18 @@ keymap.set("n", "<c-k>", "<c-w>k")
 keymap.set("n", "<c-l>", "<c-w>l")
 keymap.set("n", "<c-s>", ":wa<cr>")
 
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 keymap.set("v", "<C-r>", "hy:%s/<C-r>h//gc<left><left><left>", {desc = "Replace"})
 
