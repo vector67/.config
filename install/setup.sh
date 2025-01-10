5e148ac2dd4822dee866229aa91dfd25a47f8631
Jeffrey <jeffreymrussell@gmail.com>
2025-01-10 08:56:40 +0000

    INSTALL: add packages for eisvogel latex template to be able to compile

--- install/setup.sh
+++ install/setup.sh
@@ -196,3 +196,7 @@ brew install ripgrep
 brew tap homebrew/cask-fonts
 brew install --cask alt-tab
 
+# Install the latex stuff for the eisvogel template to work out of the box
+eval "$(/usr/libexec/path_helper)"
+sudo tlmgr update --self
+sudo tlmgr install adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref
