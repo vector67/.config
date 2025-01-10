5f2a6d7636f2cf2dc12c367f3a6cfc818d6be1f2
Jeffrey <jeffreymrussell@gmail.com>
2025-01-10 08:57:48 +0000

    NVIM: finally got pyright lsp to work and added keymaps for it

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,4 @@ vim.cmd("let &packpath = &runtimepath")
 --vim.cmd("source ~/.vimrc")
 require("jeffrey.core")
 require("jeffrey.lazy")
+require'lspconfig'.pyright.setup{}
--- nvim/lua/jeffrey/core/keymaps.lua
+++ nvim/lua/jeffrey/core/keymaps.lua
@@ -3,12 +3,16 @@ vim.g.maplocalleader= ","
 
 local keymap = vim.keymap
 
+-- Toggle fold
 keymap.set("n", " ", "za")
+
 -- Find stuff
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>" )
 keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+keymap.set("n", "<leader>fe", "<cmd>lua vim.lsp.buf.references()<CR>", { desc = "Find references", noremap = true, silent = true })
+keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition" })
 
 -- Run stuff
 keymap.set("n", "<leader>re", ":source ~/.vimrc<cr>", {desc = "Reload vimrc"})
