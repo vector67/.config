629806b183894f5f216d44be4e67e071ab69dd98
Jeffrey <jeffreymrussell@gmail.com>
2025-02-20 16:27:13 +0000

    NVIM: comment string for structurizr dsl

--- nvim/lua/jeffrey/plugins/comment.lua
+++ nvim/lua/jeffrey/plugins/comment.lua
@@ -1,17 +1,20 @@
 return {
-    "numToStr/Comment.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    config = function()
+	"numToStr/Comment.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		local comment = require("Comment")
 
-        local comment = require("Comment")
+		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
-        local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+		local ft = require("Comment.ft")
+		-- ft.set("dsl", { "#%s", "/*%s*/" })
+		ft.set("structurizr", { "#%s", "/*%s*/" })
 
-        comment.setup({
-            pre_hook = ts_context_commentstring.create_pre_hook(),
-        })
-    end,
+		comment.setup({
+			pre_hook = ts_context_commentstring.create_pre_hook(),
+		})
+	end,
 }
