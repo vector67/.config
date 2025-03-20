f9150489561e21fd2cc7c95eed1e8b15213bf428
Jeffrey <jeffreymrussell@gmail.com>
2025-03-20 13:27:26 +0000

    TMUX: auto save finally works the way I wanted it to.

--- tmux/autosave-sessions.zsh
+++ tmux/autosave-sessions.zsh
@@ -4,4 +4,13 @@ export PATH=/opt/homebrew/bin:$PATH
 
 source ~/.config/zsh/.zshrc
 
-tmux-session save
+number=$(ps aux | grep -v grep | grep -c tmux)
+
+if pgrep -x tmux >/dev/null; then
+  tmux-session save
+else
+  if [[ -s ~/.tmux-session ]]; then  # Check if the file exists and is not empty
+    echo "Restoring";
+    tmux-session restore
+  fi
+fi
