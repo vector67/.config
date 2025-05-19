d3933cda4ca0d3ca1b14a784d8473f8121bdde26
Jeffrey <jeffreymrussell@gmail.com>
2025-05-19 11:11:57 +0100

    NVIM: vimwiki, add auto tags

--- nvim/lua/jeffrey/plugins/vim-wiki.lua
+++ nvim/lua/jeffrey/plugins/vim-wiki.lua
@@ -1,10 +1,11 @@
-return { 
-    "vimwiki/vimwiki",
-    init = function()
-      vim.g.vimwiki_list = {{
-        syntax = 'markdown', 
-        exts = 'wiki',
-      }}
-      vim.g.vimwiki_folding = 'expr'
-    end
+return {
+	"vimwiki/vimwiki",
+	init = function()
+		vim.g.vimwiki_list = { {
+			syntax = "markdown",
+			exts = "wiki",
+			auto_tags = 1,
+		} }
+		vim.g.vimwiki_folding = "expr"
+	end,
 }
