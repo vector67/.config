14b588c3645d03d05970a9e269c4657e713e7bcd
vector67 <jeffreymrussell@gmail.com>
2026-01-20 15:40:55 +0000

    NVIM: turn off auto open of the markdown preview

--- nvim/lua/jeffrey/plugins/markdown-preview.lua
+++ nvim/lua/jeffrey/plugins/markdown-preview.lua
@@ -5,7 +5,7 @@ return {
   init = function()
     vim.g.mkdp_filetypes = { "markdown", "vimwiki" }
     vim.g.mkdp_images_path = "/Users/etrnj73/vimwiki/"
-    vim.g.mkdp_auto_start = 1
+    vim.g.mkdp_auto_start = 0
     vim.g.mkdp_auto_close = 0
     vim.g.mkdp_combine_preview = 1
     vim.g.mkdp_combine_preview_auto_refresh = 1
